72f53dd9a3b13c8a14c2961a37ab0c59804eb446
Hung Tran <hung_duy_tran@yahoo.com>
2015-11-18 15:42:01 -0800

    Changed repositories to repository

---
+++ API/medicloudAPI/src/main/java/repository/User_repo.java
@@ -0,0 +1,13 @@
+package repository;
+
+import model.User;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Qualifier(value = "User_repo")
+public interface User_repo extends CrudRepository<User, String> {
+    public User findByUsername(String username);
+    public User findByUserID(int userID);
+}
