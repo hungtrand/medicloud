d1b4160900017d129a50835da23b23ad3a0640f5
Hung Tran <hung_duy_tran@yahoo.com>
2015-11-18 17:31:11 -0800

    sql file for creating table person

--- Database/Tables/person.sql
+++ Database/Tables/person.sql
@@ -1,4 +1,10 @@
-create table person (
-personId int,
-firstName varchar(255)
-);
+SELECT * FROM medicloud.person;CREATE TABLE `person` (
+  `person_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `birthdate` date DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `ssn` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`person_id`),
+  UNIQUE KEY `person_id_UNIQUE` (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
