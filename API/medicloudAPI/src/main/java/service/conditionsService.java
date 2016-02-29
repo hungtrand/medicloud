cc74f6d77fbfa8ae24bb1969b97d12ac24ad4060
Hung Tran <hung_duy_tran@yahoo.com>
2016-02-28 16:44:25 -0800

    conditionService talks to Infermedica

---
+++ API/medicloudAPI/src/main/java/service/conditionsService.java
@@ -0,0 +1,83 @@
+package service;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import model.Condition;
+import provider.MessageResponse;
+import repository.ConditionRepo;
+
+@Configuration
+@PropertySource("classpath:client-side.properties")
+@RestController
+public class conditionsService {
+	@Autowired
+	private ConditionRepo condRepo;
+	
+	@Value("${infermedica.appId}")
+	private String appId;
+	
+	@Value("${infermedica.appKey}")
+	private String appKey;
+	
+	@RequestMapping(value="/conditions", method=GET)
+	public Iterable<Condition> lookupConditions() 
+	{
+		String sURL = "https://api.infermedica.com/v2/conditions"; //just a string
+
+	    // Connect to the URL using java's native library
+	    URL url;
+		try {
+			url = new URL(sURL);
+			
+			HttpURLConnection request;
+			
+			request = (HttpURLConnection) url.openConnection();
+			request.setRequestProperty("app_id", this.appId);
+			request.setRequestProperty("app_key", this.appKey);
+			request.connect();
+			
+			// Convert to a JSON object to print data
+		    JsonParser jp = new JsonParser(); //from gson
+		    JsonElement root;
+		    
+			root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+			JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object. 
+			System.out.println(rootobj.toString());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	    
+	    //zipcode = rootobj.get("zip_code").getAsString(); //just grab the zipcode
+	    
+		Iterable<Condition> listConditions = condRepo.findAll();
+		return listConditions;
+		
+	}
+}
