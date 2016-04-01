fe0bc0ad8d9c3a4582fac46ec543e10fda5f5097
Helen Tsui <helentsui@Helens-iMac.local>
2016-03-31 21:23:44 -0700

    Calendar and patient dashboard integrated with ng-view

--- Client-side/PatientCenter/patientCenter.service.js
+++ Client-side/PatientCenter/patientCenter.service.js
@@ -1,9 +1,3 @@
 module.exports = function ($http, $resource) {
-	var service = {
-		patientSignUp: function(patient) {
-			var client = $resource('http://'+window.location.hostname+':8080/user/patientSignUp');
-			client.save(patient);
-		}
-	}
-	return service;
+
 }
