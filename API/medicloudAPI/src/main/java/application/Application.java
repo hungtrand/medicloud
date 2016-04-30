b6aae978523aea0008adf57aa44f0fec7ff0365c
Kyaw <skyversal@gmail.com>
2016-04-30 11:11:02 -0700

    added API documentation

--- API/medicloudAPI/src/main/java/application/Application.java
+++ API/medicloudAPI/src/main/java/application/Application.java
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.config.RepositoryConfiguration;
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 
 @Configuration
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableJSONDoc
 @ComponentScan(basePackages = {"application","model", "repository","provider", "service", "healthProfessionalService", "patientService", "personService"})
 @EnableJpaRepositories(basePackages = "repository")
 @EntityScan(basePackages = "model")
