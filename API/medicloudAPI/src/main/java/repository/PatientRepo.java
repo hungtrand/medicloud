2031edc6c946a6be3db18713a44c5348e0a0ccb9
hhelentsuii@gmail.com <helentsui@Helens-MacBook-Pro.local>
2016-05-01 16:21:41 -0700

    PatientRepo

--- API/medicloudAPI/src/main/java/repository/PatientRepo.java
+++ API/medicloudAPI/src/main/java/repository/PatientRepo.java
@@ -14,7 +14,7 @@ import model.Person;
 public interface PatientRepo extends CrudRepository<Patient, String>{
 
 	public Patient findByHpIdAndPatientId(int hpId, int patientId);
-	public List<Patient> findByPatientId(int patientId);
+	public Patient findByPatientId(int patientId);
 	public Patient findByPerson(Person person);
 	public List<Patient> findByPersonId(int personId);
 	public List<Patient>findByPatientIdAndPersonId(int patientId, int personId);
